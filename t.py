cf4fd006f8d533ec2f61e2aeb94161605c319265
Hui Zhou <hzhou@hzsolutions.net>
2017-09-17 17:42:09 -0500

    new file:   t.py 	new file:   t.sh

---
+++ t.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1,100)
+# print(number)
+while 1:
+    print("make a guess:")
+    t = input()
+    t = int(t)
+    if t<number:
+        print("too small")
+    elif t>number:
+        print("too big")
+    else:
+        print("you got it!")
+        break
+
