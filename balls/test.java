127e69f672a833f809530f75cc04b48ae62de355
Hui Zhou <hzhou@hzsolutions.net>
2017-11-12 14:55:50 -0600

    modified:   test.java

--- balls/test.java
+++ balls/test.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 
 public class test {
     private JFrame frame;
+    private JPanel canvas;
 
     public test(){
         frame = new JFrame("NoTitle");
@@ -14,6 +15,15 @@ public class test {
             }
         });
         frame.setLayout(new FlowLayout());
+        canvas = new JPanel(){
+            public void paintComponent(Graphics g0){
+                super.paintComponent(g0);
+                Graphics2D g = (Graphics2D)g0;
+                g.fillOval(0, 0, 20, 20);
+            }
+        };
+        canvas.setPreferredSize(new Dimension(500, 500));
+        frame.add(canvas);
         frame.setSize(500, 500);
         frame.setVisible(true);
     }
