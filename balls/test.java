14350f20230592ab77cc7a9197616ce76704e682
Hui Zhou <hzhou@hzsolutions.net>
2017-11-12 20:01:40 -0600

    new file:   test.def 	modified:   test.java

--- balls/test.java
+++ balls/test.java
@@ -6,8 +6,9 @@ import javax.swing.Timer;
 
 public class test {
     private JFrame frame;
-    private int x;
-    private int y;
+    private double f_theta;
+    private double f_dx;
+    private double f_dy;
     private JPanel canvas;
     private Timer timer;
 
@@ -19,20 +20,26 @@ public class test {
             }
         });
         frame.setLayout(new FlowLayout());
-        x = 200;
-        y = 100;
+        f_theta = 0;
+        f_dx = 0;
+        f_dy = 5;
         canvas = new JPanel(){
             public void paintComponent(Graphics g0){
                 super.paintComponent(g0);
                 Graphics2D g = (Graphics2D)g0;
+                int x;
+                int y;
+
+                x = 250 + (int)(100 * Math.cos(f_theta));
+                y = 250 + (int)(100 * Math.sin(f_theta));
                 g.fillOval(x, y, 20, 20);
             }
         };
         canvas.setPreferredSize(new Dimension(500, 500));
         frame.add(canvas);
-        timer = new Timer(1000/5, new ActionListener(){
+        timer = new Timer(1000/60, new ActionListener(){
             public void actionPerformed(ActionEvent evt){
-                y += 5;
+                f_theta += 0.01;
                 canvas.repaint();
             }
         });
